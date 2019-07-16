dc11762d66b578b9a6ce511479fe9b11a2ae7bbc
Oscar Olivares <oscarolivares625@gmail.com>
2019-07-16 16:05:13 -0400

    Trabajando con modulos propios de NodeJS en este caso se prueba el modulo 'os'

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-const math = require('./math');
+// Modulo con funciones del OS
+const os = require('os');
 
-console.log(math);
+console.log('Total mem', (os.totalmem()/1073741824).toPrecision(2), 'Gb')
+console.log('Free mem', (os.freemem()/1073741824).toPrecision(2), 'Gb');
