f7d15744fb8bac0ab2b4e962a9fed77cc7458dc4
Oscar Olivares <oscarolivares625@gmail.com>
2019-07-16 17:27:31 -0400

    Creando un servidor sencillo y luego importando un modulo

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
-const fs = require('fs');
+/* const http = require('http');
 
-// Codigo asincrono con callback 'fuction(err)'
-fs.writeFile('./texto.txt', 'linea uno', function(err) {
-  if(err) {
-    console.log(err);
-  } else {
-    console.log('Archivo creado');
-    
-  }
+http.createServer(function(req, res) {
+  res.writeHead(200, {'Content-type': 'text/html'});
+  res.write('<h1>Hola mundo desde NodeJS</h1>');
+  res.end();
+}).listen(3000); */
+
+// Refactorizando un poco codigo
+/* const http = require('http');
+
+const handlerServer = function(req, res) {
+  res.writeHead(200, {'Content-type': 'text/html'});
+  res.write('<h1>Hola mundo</h1>');
+  res.end();
+}
+
+const server = http.createServer(handlerServer);
+
+server.listen(3000, function() {
+  console.log('Server on port 3000');
+}); */
+
+// Agregando un modulo con NPM, en este caso "colors"
+// para darle algunos stilos a los mensajes por consola
+const http = require('http');
+const clors = require('colors');
+
+const handlerServer = function(req, res) {
+  res.writeHead(200, {'Content-type': 'text/html'});
+  res.write('<h1>Hola mundo</h1>');
+  res.end();
+}
+
+const server = http.createServer(handlerServer);
+
+server.listen(3000, function() {
+  console.log('Server on port 3000'.yellow);
 });
-
-console.log('codigo siguiente');
